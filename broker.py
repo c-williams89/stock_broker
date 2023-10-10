5d8f3be17c90c106d4e22f6eb39e41e34dd80468
clayton williams <clayton.e.williams15.mil@mail.mil>
2023-10-10 15:08:30 -0400

    pycodestyle compliant on broker.

--- broker.py
+++ broker.py
@@ -4,30 +4,31 @@ from random import gauss
 import json
 import decimal
 
+
 class Stock:
     def __init__(self, ticker, name, start_price, std_dev, av_vol):
         self._ticker = ticker
         self._name = name
         self._start_price = start_price
-        self._volatility : Tuple(float) = (av_vol, std_dev)
-        self._price : List(float) = [start_price]
+        self._volatility: Tuple(float) = (av_vol, std_dev)
+        self._price: List(float) = [start_price]
 
     @property
     def ticker(self):
         return self._ticker
-    
+
     @property
     def name(self):
         return self._name
-    
+
     @property
     def price(self):
         return self._price[-1]
-    
+
     @property
     def history(self):
         return self._price
-    
+
     def increment_price(self):
         price_change = gauss(self._volatility[0], self._volatility[1])
         new_price = self._price[-1] + price_change
